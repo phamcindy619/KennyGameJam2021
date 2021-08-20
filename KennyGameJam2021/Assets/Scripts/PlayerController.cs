aec782226aa4a75bfb6b199719f9e0a38a3738bc
Cindy Pham <phamcindy619@utexas.edu>
2021-08-20 12:13:43 -0500

    Finish player controls

--- KennyGameJam2021/Assets/Scripts/PlayerController.cs
+++ KennyGameJam2021/Assets/Scripts/PlayerController.cs
@@ -4,8 +4,8 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    [SerializeField]
-    public float speed;
+    public float speed = 5f;
+    public float moveLimiter = 0.7f;
     
     private float h, v;
     private Rigidbody2D rb;
@@ -20,10 +20,23 @@ public class PlayerController : MonoBehaviour
     {
         h = Input.GetAxisRaw("Horizontal");
         v = Input.GetAxisRaw("Vertical");
+
+
     }
 
     private void FixedUpdate() {
+        // Moving diagonally
+        if (h != 0 && v != 0) {
+            // Limit diagonal movement speed
+            h *= moveLimiter;
+            v *= moveLimiter;
+        }
+
         rb.velocity = new Vector2(h * speed, v * speed);
-    }
-    
+
+        if (rb.velocity != Vector2.zero) {
+            float angle = Mathf.Atan2(rb.velocity.y, rb.velocity.x) * Mathf.Rad2Deg;
+            transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+        }
+    }  
 }
